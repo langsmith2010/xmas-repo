70c4e19b43cdedd998a4ff98ecfaa806a054f1fd
private <langsmith2010@gmail.com>
2023-12-25 13:27:30 -0500

    deleting some files

--- main.tf
+++ main.tf
@@ -6,19 +6,12 @@ data "aws_ami" "ubuntu" {
     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
 
-  owners = ["099720109477"] # Canonical
-}
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
 
-  tags = {
-    Name = "HelloWorld"
+  
   }
 }
--- provider.tf
+++ provider.tf
@@ -6,3 +6,7 @@ terraform {
     }
   }
 }
+provider "aws" {
+    # Configuration options 
+    region = "us-east-1"
+}
